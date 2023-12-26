6ae00832a34555acd1ed6e5f0753e450dc3e1430
hk-none <68328936+chk-n@users.noreply.github.com>
2023-12-27 00:53:03 +0100

    Close channel in context

--- context.go
+++ context.go
@@ -16,11 +16,12 @@ func (c *Context) Done() chan any {
 // Cancels message or grouped message. Multiple calls to this function wont cause errors
 func (c *Context) Cancel() {
 	atomic.SwapInt32(&c.cancel, 1)
+	close(c.ch)
 }
 
 func (c *Context) done() {
 	v := atomic.AddInt64(&c.cnt, 1)
-	if v >= c.trigger {
+	if v >= c.trigger && !c.canceled() {
 		c.ch <- struct{}{}
 	}
 }
